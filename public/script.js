d24eef254890bbd90a915cce90ffc36ac517f131
Pih Lung Pang <pihlung.pang@gmail.com>
2018-01-01 09:03:24 -0700

    Vue-Resource Uncaught TypeError: Cannot read property 'get' of undefined

--- public/script.js
+++ public/script.js
@@ -1,1 +1,75 @@
-console.log('It works.');
+// Vue constructor
+// this ref to Vue object
+var PRICE = 9.99;
+
+new Vue({
+	el: '#app',
+	data: {
+		total: 0,
+		items: [
+		  {id: 1, title: 'Item 1'},
+		  {id: 2, title: 'Item 2'},
+		  {id: 3, title: 'Item 3'},
+		],
+		cart: [],
+		search: ''
+
+	},
+	methods: {
+		onSubmit: function(){
+			this.$http
+			.get('/search/'.concat('90s'))
+			.then(function(res){
+				console.log(res);
+			});
+
+		},
+		addItem: function(index) {
+			this.total += 9.99;
+			var item = this.items[index];
+			var found = false;
+			for(var i = 0; i< this.cart.length;i++){
+				if(this.cart[i].id === item.id){
+					found = true;
+					this.cart[i].qty++;
+					break;
+				}
+			}
+			if(!found){
+			this.cart.push({
+				id: item.id,
+				title: item.title,
+				qty:1,
+				price: PRICE
+				});    //push an object
+			}
+			// this.cart.push(this.items[index]);
+			// console.log(this.cart.lenght);
+		},
+		inc: function(item){
+			// console.log("inc");
+			item.qty++;
+			this.total += PRICE;
+		},
+		dec: function(item){
+			// console.log("dec");
+			item.qty--;
+			this.total -= PRICE;
+			if(item.qty <= 0){
+				for(var i=0;i< this.cart.length; i++){
+					if (this.cart[i].id === item.id){
+						this.cart.splice(i,1);
+						break;
+					}
+				}
+			}
+		}
+	},
+	filters: {
+		currency: function(price){
+			return '$'.concat(price.toFixed(2)); //JS string method toFixed
+		}
+	}
+
+});
+
